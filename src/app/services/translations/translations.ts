592f4e85b2d96d6d37859bad2bd33e3b9c882c7b
musialpatryk <musial.patryk10@gmail.com>
2021-12-30 20:11:43 +0100

    Changes: - make menu responsive, - add menu translations

--- src/app/services/translations/translations.ts
+++ src/app/services/translations/translations.ts
@@ -1,12 +1,22 @@
-
-
 const EN = {
   lang: 'en',
   translations: [
     {
       key: 'LOGIN',
       value: 'Login'
-    }
+    },
+    {
+      key: 'NOTES',
+      value: 'Notes'
+    },
+    {
+      key: 'GROUP_MANAGEMENT',
+      value: 'Group management'
+    },
+    {
+      key: 'ACCOUNT',
+      value: 'Account'
+    },
   ]
 };
 
