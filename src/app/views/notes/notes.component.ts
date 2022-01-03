7f8dd8137722c4c829fcfd57753e49ee8d1cb554
musialpatryk <musial.patryk10@gmail.com>
2022-01-03 21:44:07 +0100

    Add notes presentation

--- src/app/views/notes/notes.component.ts
+++ src/app/views/notes/notes.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
 
+interface INote {
+  id: number;
+  title: string;
+  description: string;
+  priority: number;
+}
+
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent {
+  notes: INote[];
+
+  constructor() {
+    this.notes = this.getMockNotes(10);
+  }
+
+  private getMockNotes(count: number): INote[] {
+    const notes = [];
+    for (let i = 0; i < count; i++) {
+      notes.push({
+        id: i,
+        title: 'note_' + i,
+        description: 'note_content' + i,
+        priority: i
+      });
+    }
+    return notes;
+  }
 }
