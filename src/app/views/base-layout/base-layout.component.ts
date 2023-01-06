344f2e0a53c9d358c1f826493f4b782b508ab0a8
musialpatryk <musial.patryk10@gmail.com>
2023-01-06 21:56:12 +0100

    Privileges

--- src/app/views/base-layout/base-layout.component.ts
+++ src/app/views/base-layout/base-layout.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user/user.service';
+import {IRestUser} from 'src/app/services/rest/rest.interfaces';
+
+interface ILink {
+  link: string;
+  lang: string;
+  roles: IRestUser['roles'];
+}
 
 @Component({
   selector: 'app-base-layout',
   templateUrl: './base-layout.component.html',
 })
-export class BaseLayoutComponent  {
+export class BaseLayoutComponent implements OnInit {
+  links: ILink[] = [
+    {
+      link: 'notes',
+      lang: 'NOTES',
+      roles: ['user']
+    },
+    {
+      link: 'groups',
+      lang: 'GROUP_MANAGEMENT',
+      roles: ['user']
+    },
+    {
+      link: 'account',
+      lang: 'ACCOUNT',
+      roles: ['user']
+    },
+    {
+      link: 'admin',
+      lang: 'ADMIN',
+      roles: ['admin']
+    },
+  ];
+  availableLinks: ILink[] = [];
+
   constructor(
     public userService: UserService,
   ) {
+  }
+
+  ngOnInit(): void {
+    const user = this.userService.getUser();
+    this.availableLinks = this.links.filter((link) => {
+      return user.roles.find((role) => {
+        return link.roles.includes(role);
+      })
+    });
   }
 }
